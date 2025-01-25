869f05c3e38dbbb5f7604324561b5908ca60c84c
abdm149 <abualrobabd@gmail.com>
2025-01-25 14:35:30 +0400

    Products and Routing setup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,24 @@
-import {Nav, Container , Navbar } from "react-bootstrap";
+import { Nav, Container, Navbar } from "react-bootstrap";
 import { FaUser, FaShoppingCart } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="ProShop" width="50" height="50" />
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FaStar, FaStarHalfAlt, } from 'react-icons/fa'
+const Rating = () => {
+  return (
+    <div>Rating</div>
+  )
+}
+
+export default Rating
